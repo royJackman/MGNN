201ec7af4aa77bd8ae9138f5cd90f9ab1c4359e1
Roy Jackman <jackman.roy@gmail.com>
2021-03-31 15:59:43 -0400

    Better RDTSC benchmark

--- RDTSCBenchmark.py
+++ RDTSCBenchmark.py
@@ -2,16 +2,22 @@ import argparse
 import ast
 import rdtsc
 
+import numpy as np
+
 parser = argparse.ArgumentParser(description='Benchmarking cycles for a program')
 parser.add_argument('-f', '--file', dest='filename', type=str, help='File to benchmark')
+parser.add_argument('-i', '--iterations', dest='iterations', type=int, help='Number of times to run compiled code')
 args = parser.parse_args()
 
 with open(args.filename, 'r') as f:
     tree = ast.parse(f.read())
 
 compiled = compile(tree, filename="<ast>", mode="exec")
-
-start = rdtsc.get_cycles()
-exec(compiled)
-end = rdtsc.get_cycles()
-print(f'Cycles: {end - start}')
+cycles = []
+for i in range(args.iterations):
+    start = rdtsc.get_cycles()
+    exec(compiled)
+    end = rdtsc.get_cycles()
+    cycles.append(end - start)
+cycles = np.array(cycles)
+print(f'For {args.iterations} cycles\nMean:\t{np.mean(cycles)}\tMax:\t{np.max(cycles)}\tMin:\t{np.min(cycles)}\tStd:\t{np.std(cycles)}\tVar:\t{np.var(cycles)}')
